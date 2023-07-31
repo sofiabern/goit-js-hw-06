b3d43f73c540f91deaf8c5483ce88ecf4c3cb4cd
sofiabern <cauliflowers33@gmail.com>
2023-07-31 23:07:09 +0300

    do the third task

--- js/task-02.js
+++ js/task-02.js
@@ -12,7 +12,7 @@ const list = document.querySelector("#ingredients");
 const markup = ingredients.map((ingredient) => {
   const item = document.createElement("li");
   item.textContent = ingredient;
-  item.classList = "item";
+  item.classList.add("item");
   return item;
 });
 list.append(...markup);
